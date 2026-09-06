dc8463be70c26eae731351903b6c29bc321084e8
harishpixcel-debug <harish.pixcel@gmail.com>
2026-09-06 14:36:43 +0530

    Update InquiryForm.tsx

--- components/InquiryForm.tsx
+++ components/InquiryForm.tsx
@@ -14,7 +14,7 @@ export default function InquiryForm() {
     const data = new FormData(form);
 
     try {
-      const res = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const res = await fetch("https://formspree.io/f/myeydwvd", {
         method: "POST",
         body: data,
         headers: { Accept: "application/json" },
